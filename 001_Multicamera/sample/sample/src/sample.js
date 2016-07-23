8886c4bf0e15c1b4d6304e9b8ad59adc46427732
nn4343 <gea01441@nifty.com>
2016-07-23 20:00:42 +0900

    ok

--- 001_Multicamera/sample/sample/src/sample.js
+++ 001_Multicamera/sample/sample/src/sample.js
@@ -2,22 +2,25 @@ var Sample;
 (function (Sample) {
     var MainClass = (function () {
         function MainClass(canvas) {
-            this.engine = new BABYLON.Engine(canvas, true);
-            this.scene = new BABYLON.Scene(this.engine);
-            this.scene.clearColor = new BABYLON.Color3(56 / 256, 87 / 256, 145 / 256);
-            this.camera = new BABYLON.ArcRotateCamera("camera1", 0, 0.8, 100, BABYLON.Vector3.Zero(), this.scene);
-            this.camera.attachControl(canvas);
-            var mesh = BABYLON.Mesh.CreateBox("box01", 50, this.scene);
-            var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.scene);
+            this.m_engine = new BABYLON.Engine(canvas, true);
+            this.m_scene = new BABYLON.Scene(this.m_engine);
+            this.m_scene.clearColor = new BABYLON.Color3(56 / 256, 87 / 256, 145 / 256);
+            this.m_camera1 = new BABYLON.ArcRotateCamera("camera1", 0, 0.8, 100, BABYLON.Vector3.Zero(), this.m_scene);
+            this.m_camera1.attachControl(canvas);
+            var mesh = BABYLON.Mesh.CreateBox("box01", 50, this.m_scene);
+            this.m_camera2 = new BABYLON.ArcRotateCamera("camera2", 0, 0.8, -100, new BABYLON.Vector3(-100, 0, 0), this.m_scene);
+            this.m_camera2.attachControl(canvas);
+            this.m_scene.activeCameras.push(this.m_camera1);
+            this.m_scene.activeCameras.push(this.m_camera2);
+            var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.m_scene);
         }
         MainClass.prototype.runRenderLoop = function () {
             var _this = this;
-            this.engine.runRenderLoop(function () {
-                _this.scene.render();
+            this.m_engine.runRenderLoop(function () {
+                _this.m_scene.render();
             });
         };
         return MainClass;
     }());
     Sample.MainClass = MainClass;
 })(Sample || (Sample = {}));
-//# sourceMappingURL=sample.js.map
--- 001_Multicamera/sample/sample/src/sample.ts
+++ 001_Multicamera/sample/sample/src/sample.ts
@@ -1,28 +1,36 @@
 ﻿module Sample {
     export class MainClass {
-        public engine: BABYLON.Engine;
-        public scene: BABYLON.Scene;
-        public camera: BABYLON.TargetCamera;
+        public m_engine: BABYLON.Engine;
+        public m_scene: BABYLON.Scene;
+        public m_camera1: BABYLON.TargetCamera;
+        public m_camera2: BABYLON.TargetCamera;
 
         constructor(canvas: HTMLCanvasElement) {
 
-            this.engine = new BABYLON.Engine(canvas, true);
+            this.m_engine = new BABYLON.Engine(canvas, true);
 
-            this.scene = new BABYLON.Scene(this.engine);
+            this.m_scene = new BABYLON.Scene(this.m_engine);
 
-            this.scene.clearColor = new BABYLON.Color3(56 / 256, 87 / 256, 145 / 256);
+            this.m_scene.clearColor = new BABYLON.Color3(56 / 256, 87 / 256, 145 / 256);
 
-            this.camera = new BABYLON.ArcRotateCamera("camera1", 0, 0.8, 100, BABYLON.Vector3.Zero(), this.scene);
-            this.camera.attachControl(canvas);
+            this.m_camera1 = new BABYLON.ArcRotateCamera("camera1", 0, 0.8, 100, BABYLON.Vector3.Zero(), this.m_scene);
+            this.m_camera1.attachControl(canvas);
 
-            var mesh = BABYLON.Mesh.CreateBox("box01", 50, this.scene);
+            var mesh = BABYLON.Mesh.CreateBox("box01", 50, this.m_scene);
 
-            var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.scene);
+            this.m_camera2 = new BABYLON.ArcRotateCamera("camera2", 0, 0.8, -100, new BABYLON.Vector3(-100,0,0), this.m_scene);
+            this.m_camera2.attachControl(canvas);
+            
+            this.m_scene.activeCameras .push(this.m_camera1);
+            this.m_scene.activeCameras .push(this.m_camera2);
+
+
+            var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.m_scene);
 
         }
         public runRenderLoop(): void {
-            this.engine.runRenderLoop(() => {
-                this.scene.render();
+            this.m_engine.runRenderLoop(() => {
+                this.m_scene.render();
             });
         }
     }
